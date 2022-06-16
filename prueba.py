b58b729c490121a957ae50a812ee227d21553507
MateoCostantini <101668925+MateoCostantini@users.noreply.github.com>
2022-06-15 21:02:26 -0300

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -7,16 +7,31 @@ Created on Tue Jun 14 09:12:18 2022
 import funciones
 import numpy as np
 import matplotlib.pyplot as plt
-import piano
 
-param = funciones.TRI(0.07, 0.02, 1.3)
+param = ["TRI", 0.07, 0.02, 1.3]
+param1 = ["EXP", 0.07]
+param2 = ["INVLINEAR", 0.07]
+
 duration = 0.07
 fs = 44100
 
 
 def get_func(param, duration, fs):
     ts = 1/fs
-    t = param[0](np.arange(0, 0.07, ts))
-    return plt.plot(t)
+    t = np.arange(0, 0.07, ts)
+    if param[0] == "CONSTANT":
+        array = funciones.CONSTANT()
+    elif param[0] == "LINEAR":
+        array = funciones.LINEAR(param[1])
+    elif param[0] == "EXP":
+        array = funciones.EXP(t, param[1])
+    elif param[0] == "TRI":
+        array = funciones.TRI(t, param[1], param[2], param[3], ts)
+    elif param[0] == "INVLINEAR":
+        array1 = funciones.INVLINEAR(t, param[0])
+    return array
 
-get_func(param, duration, fs)
+print(get_func(param, duration, fs))
+plt.plot(get_func(param, duration, fs))
+
+
