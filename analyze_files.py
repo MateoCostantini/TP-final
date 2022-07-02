8b596256a7342633d8be47ffff2f181d40e21b98
MateoCostantini <101668925+MateoCostantini@users.noreply.github.com>
2022-07-02 18:16:28 -0300

    Add files via upload

--- analyze_files.py
+++ analyze_files.py
@@ -4,11 +4,12 @@ Created on Sat Jul  2 15:19:08 2022
 
 @author: 54911
 """
+import notes
 
 def get_instrument():
     harmonics_list = []
     module_list = []
-    with open("flute.txt", "r") as f:
+    with open("piano.txt", "r") as f:
         line = f.readline()
         line = int(line)
         for i in range(line):
@@ -30,6 +31,10 @@ def get_partiture():
     with open("partiture.txt", "r") as f:
         for line in f:
             note = (line.rstrip()).split(" ")
+            for i in range(len(notes.notes_mapping)):
+                if note[1] == notes.notes_mapping[i][0]:
+                    note[1] = notes.notes_mapping[i][1]
+                    break
             note[0], note[2] = float(note[0]), float(note[2])
             partiture.append(note)
     return partiture
