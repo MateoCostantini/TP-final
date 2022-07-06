dec97d3a130c696b773f392720f0a8f00245ae0a
Manuvaliante <103191043+Manuvaliante@users.noreply.github.com>
2022-07-06 11:53:16 -0300

    Add files via upload

--- Test_main.py
+++ Test_main.py
@@ -11,7 +11,7 @@ def test_notes():
     fs = 44100
     partiture_file = open('partiture.txt', 'r')
     instrument_file = piano
-    harmonics_list, module_list = [(1, 1),(2, 1), (3, 0), (4, 1)], [('LINEAR', 0.02), ('CONSTANT'), ('INVEXP', 0.06)]
+    harmonics_list, module_list = [(1, 1),(2, 1), (3, 0), (4, 1)], [('LINEAR', 0.02), ('CONSTANT',), ('INVEXP', 0.06)]
     partiture = [(0, 261.626, 0.5), (0.5, 293.665, 0.5), (1, 329.628, 0.5), (1.2, 261.626, 0.7)]
     track = 86417
     partiture_file.close()
